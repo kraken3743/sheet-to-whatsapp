d0953b6e6a79e1568b1f33acc800620e1b69cc6a
Shashwat Amrit <shashwatamrit@Shashwats-MacBook-Air.local>
2025-07-03 21:59:19 +0530

    Run scheduler and web in same process

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
-import threading
 from scheduler import schedule_user, run_loop
+import threading
 import os
 
 app = Flask(__name__)
@@ -24,6 +24,9 @@ def register():
         return "Failed to schedule.", 500
 
 if __name__ == '__main__':
+    # START scheduler loop as background thread inside web server
     threading.Thread(target=run_loop, daemon=True).start()
-    port = int(os.environ.get('PORT', 5000))
+    
+    # Start Flask server on correct port for Render
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
